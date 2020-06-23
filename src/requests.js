8a2de31f68f1e4825129a5711d7807dcd064975d
CBreakr <github email address>
2020-06-23 12:18:52 -0400

    updated URL for requests

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000";
+// const baseURL = "http://localhost:3000";
+const baseURL = "https://cookbook-social-server.herokuapp.com";
 
 const handleError = err => console.error(err);
 
