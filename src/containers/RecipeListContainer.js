6d6561b04ecc15876bd4eaed4e10b951e26e06e8
CBreakr <github email address>
2020-06-26 15:50:04 -0400

    test message

--- src/containers/RecipeListContainer.js
+++ src/containers/RecipeListContainer.js
@@ -94,6 +94,7 @@ class RecipeListContainer extends React.Component {
     render(){
         console.log('🔫Recipe container, state:', this.state)
         console.log("recipe list props", this.props);
+        console.log("test message");
         return (
             <div>RECIPE</div>
         )
